03b4ac38b770ad9b5c7cdcb172965c89eedc7743
aivanius <aivanius@yandex.ru>
2026-02-09 00:46:54 +0700

    09.02.26-email-failed-changes

--- app/api/support/route.tsx
+++ app/api/support/route.tsx
@@ -1,47 +1,40 @@
 import { NextRequest, NextResponse } from "next/server"
 import nodemailer from "nodemailer"
 
+export const runtime = 'nodejs'
+
 export async function POST(req: NextRequest) {
     try {
+        const { email, message } = await req.json()
 
+        if (!email || !message) {
+            return NextResponse.json({ error: "Missing fields" }, { status: 400 })
+        }
 
-    const { email, message } = await req.json()
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        })
 
-    if (!email || !message) {
-        return NextResponse.json(
-            { error: "Missing fields" },
-            { status: 400 }
-        )
-    }
+        await transporter.sendMail({
+            from: process.env.EMAIL_FROM,
+            to: process.env.EMAIL_USER,
+            subject: "Запрос в поддержку Диетикса",
+            html: `
+        <p><b>Email пользователя:</b> ${email}</p>
+        <p><b>Сообщение:</b></p>
+        <p>${message}</p>
+      `,
+        })
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
-    })
-
-    await transporter.sendMail({
-        from: process.env.EMAIL_FROM,
-        to: process.env.EMAIL_USER,
-        subject: "Запрос в поддержку Диетикса",
-        html: `
-            <p><b>Email пользователя:</b> ${email}</p>
-            <p><b>Сообщение:</b></p>
-            <p>${message}</p>
-        `,
-    })
-
-    return NextResponse.json({ success: true })
+        return NextResponse.json({ success: true })
     } catch (error) {
-        console.log(error)
-        return NextResponse.json({error:error}, {status:500})
-
+        console.error(error)
+        return NextResponse.json({ error: "Mail error" }, { status: 500 })
     }
 }
